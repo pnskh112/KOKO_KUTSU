6c8a5f7ad0f1f1ab39886c21b2a333d170222c7f
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-04 19:59:02 +0900

    pre_releace commit_0104

---
+++ db/migrate/20200103145324_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name,              limit: 191,             null: false
+      t.string :mail,              limit: 191,             null: false
+      t.string :password,          limit: 191,             null: false
+      t.string :password_digest,   limit: 191,             null: false
+      t.string :remember_token,    limit: 191,             null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_053221) do
+ActiveRecord::Schema.define(version: 2020_01_03_145324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2019_12_30_053221) do
     t.string "name"
     t.integer "max_vertical"
     t.integer "max_side"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 191, null: false
+    t.string "mail", limit: 191, null: false
+    t.string "password", limit: 191, null: false
+    t.string "password_digest", limit: 191, null: false
+    t.string "remember_token", limit: 191, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
